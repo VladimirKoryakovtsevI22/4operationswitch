fc1561b44d33a33ee869d50887114d22d3162102
VladimirKoryakovtsevI22 <157983129+VladimirKoryakovtsevI22@users.noreply.github.com>
2024-06-30 18:58:10 +0300

    Add files via upload

---
+++ 4operationswitch/4operationswitch/4operationswitch.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a;
+	char znak;
+	int b;
+
+	printf("Entered operation: ");
+	scanf_s("%d %c %d", &a, &znak, 1, &b);
+
+	switch (znak)
+	{
+	case '+':
+
+		int c;
+		c = a + b;
+		printf("Sum= %d\n", c);
+		break;
+
+	case '-':
+		
+
+		c = a - b;
+		printf("Raznoct= %d\n", c);
+		break;
+
+	case '*':
+
+
+		c = a * b;
+		printf("Ymnozenie= %d\n", c);
+		break;
+
+	case '/':
+
+
+		c = a / b;
+		printf("Delenie= %d\n", c);
+		break;
+
+
+	
+	default:
+		printf("???????");
+		break;
+	}
+}
+
+
